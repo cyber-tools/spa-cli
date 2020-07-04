3658434e028f75e84602dd53e29b8ee9f3dd0a46
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-05 01:26:50 +0800

    2020年07月05日01点26分50秒的快捷提交

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 require("module-alias").addAlias("@", __dirname);
-const path = require("path");
+const dotProp = require("dot-prop");
 const webpack = require("webpack");
 const webpackMerge = require("webpack-merge");
 const WebpckDevServer = require("webpack-dev-server");
@@ -13,6 +13,6 @@ const custmerConfig = getCustmerConfig();
 const computedConfig = webpackMerge(basicConfig, custmerConfig);
 const complier = webpack(computedConfig);
 const server = new WebpckDevServer(complier, computedConfig.devServer);
-server.listen(8080);
+server.listen(dotProp.get(computedConfig, "devServer.port") || 8080);
 
 
