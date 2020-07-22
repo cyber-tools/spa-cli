a9a807cd1b2513d2de7100dc5de430601a50704f
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-22 23:21:36 +0800

    2020年07月22日 23:36的快速提交

--- utils/getBabelConfig.js
+++ utils/getBabelConfig.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = async () => {
   const babelrcModuleResolve = require.resolve("./.babelrc.js", {
-    paths: [process.cwd(), path.join(__dirname, "configs")]
+    paths: [process.cwd(), path.resolve(__dirname, "../configs")]
   });
   const babelrc = require(babelrcModuleResolve);
   return babelrc;
